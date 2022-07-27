0b2966528a8b675531df2079657458dd2ad11199
Rohit Pandey <rohitpandey357@gmail.com>
2022-07-27 23:27:42 +0530

    Setup

---
+++ src/main/java/com/example/cabbookingservice/CabbookingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbookingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabbookingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cabbookingservice/CabbookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabbookingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
